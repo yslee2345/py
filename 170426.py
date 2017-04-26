8120fbcf8dfb4d72f705d8368f70f06e59050e83
yslee2345 <acegauss@naver.com>
2017-04-26 10:49:04 +0900

    23
    231

--- 170426.py
+++ 170426.py
@@ -11,7 +11,6 @@ print("begining of 170426.py...")
 print('name:{0}'.format(__name__))
 print("end of 170426.py...")
 
-
 #문제1. 위 메시지가 출력이 안되게 하려면?
 
 if __name__=='__main__':
@@ -19,5 +18,5 @@ if __name__=='__main__':
     print('name:{0}'.format(__name__))
     print("end of 170426.py...")
 
-
+#문제2. 패키지와 모듈을 만드려면3123123123
 
