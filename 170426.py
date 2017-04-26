b68cf034b6240782419e2b952f1d70df36c8c9e3
yslee2345 <acegauss@naver.com>
2017-04-26 10:23:08 +0900

    0

---
+++ 170426.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 26 09:55:25 2017
+
+@author: stu
+"""
+
+#예제
+
+print("begining of 170426.py...")
+print('name:{0}'.format(__name__))
+print("end of 170426.py...")
+
+
+#문제1. 위 메시지가 출력이 안되게 하려면?
+
+if __name__=='__main__':
+    print("begining of 170426.py...")
+    print('name:{0}'.format(__name__))
+    print("end of 170426.py...")
+
+
+
